aaa2e6f59dd0bfc0335d4be0f770b2924b4e16c3
LLL <1048252403@qq.com>
2020-10-14 16:28:14 +0800

    feat git-cz

--- .nuxt/router.js
+++ .nuxt/router.js
@@ -3,10 +3,11 @@ import Router from 'vue-router'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _7a0ea6c3 = () => interopDefault(import('../pages/editor.vue' /* webpackChunkName: "pages/editor" */))
-const _793f8e29 = () => interopDefault(import('../pages/diary/20200803.vue' /* webpackChunkName: "pages/diary/20200803" */))
-const _a35fe026 = () => interopDefault(import('../pages/diary/20200906.vue' /* webpackChunkName: "pages/diary/20200906" */))
-const _529ab16c = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
+const _59bb389d = () => interopDefault(import('..\\pages\\editor.vue' /* webpackChunkName: "pages/editor" */))
+const _1d7d1d1f = () => interopDefault(import('..\\pages\\markdown.vue' /* webpackChunkName: "pages/markdown" */))
+const _56fb0e0a = () => interopDefault(import('..\\pages\\diary\\20200803.vue' /* webpackChunkName: "pages/diary/20200803" */))
+const _e7e8e064 = () => interopDefault(import('..\\pages\\diary\\20200906.vue' /* webpackChunkName: "pages/diary/20200906" */))
+const _30877c52 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
 
 // TODO: remove in Nuxt 3
 const emptyFn = () => {}
@@ -26,19 +27,23 @@ export const routerOptions = {
 
   routes: [{
     path: "/editor",
-    component: _7a0ea6c3,
+    component: _59bb389d,
     name: "editor"
   }, {
+    path: "/markdown",
+    component: _1d7d1d1f,
+    name: "markdown"
+  }, {
     path: "/diary/20200803",
-    component: _793f8e29,
+    component: _56fb0e0a,
     name: "diary-20200803"
   }, {
     path: "/diary/20200906",
-    component: _a35fe026,
+    component: _e7e8e064,
     name: "diary-20200906"
   }, {
     path: "/",
-    component: _529ab16c,
+    component: _30877c52,
     name: "index"
   }],
 
---
+++ pages/markdown.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: jiarong.deng
+ * @Date: 2020-07-29 21:56:51
+ * @LastEditors: jiarong.deng
+ * @LastEditTime: 2020-10-14 16:17:26
+ * @Description: file content
+-->
+<template>
+  <div class="preview">
+  </div>
+</template> 
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { fetchFile } from '~/assets/js/utils';
+const jsonFile = require('~/assets/js/data.json');
+
+@Component
+export default class Home extends Vue {
+  public summaryArr = jsonFile.data;
+
+  mounted() {
+    this.$store.commit('article/setTitle', 'Summary');
+    fetchFile('./README.md').then(res => {
+      console.log('md....', res);
+    })
+  }
+}
+</script>
+<style lang="scss" scoped>
+.container > * {
+  margin-top: 1rem;
+}
+</style>
